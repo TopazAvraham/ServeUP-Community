31cf20a886cb9aa53f81ed573758cfaf43834ac4
topazavraham <topazavraham9@gmail.com>
2024-01-14 13:58:53 +0200

    ServeUp-19 | create basic folders hierarchy + defining levels of game

---
+++ Mobile App/helpers/levelOptions.ts
@@ -0,0 +1,10 @@
+const levelOptions = {
+    0: 'Fresh',
+    1: 'Rising',
+    2: 'Fearless',
+    3: 'Smasher',
+    4: 'Elite',
+    5: 'Legend',
+    6: 'Master',
+  };
+export default levelOptions;
