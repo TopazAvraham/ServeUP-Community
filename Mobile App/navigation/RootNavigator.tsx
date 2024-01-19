3237825ed2f2779913f091347a541c267e5a5191
topazavraham <topazavraham9@gmail.com>
2024-01-19 18:53:50 +0200

    ServeUP-19 | add buttom menu to naviagate app + creating signup continue screen for the player to choose his starting level

--- Mobile App/navigation/RootNavigator.tsx
+++ Mobile App/navigation/RootNavigator.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AuthStackScreens from "./AuthStack";
+import BottomStackScreens from "./BottomStack";
 import { NavigationContainer } from "@react-navigation/native";
 
 const RootStack = createNativeStackNavigator();
@@ -11,6 +13,8 @@ const AppNavigator = () => {
         initialRouteName={"AuthStack"}
         screenOptions={{ headerShown: false }}
       >
+        <RootStack.Screen component={AuthStackScreens} name="AuthStack" />
+        <RootStack.Screen component={BottomStackScreens} name="BottomStack" />
       </RootStack.Navigator>
     </NavigationContainer>
   );
